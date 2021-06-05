a98c0d00560123a8621f88fe59bda107bd0d3ae5
HarshBrownsVI <harshaaitharaju15@gmail.com>
2021-06-05 14:44:20 -0400

    Exercise 2

---
+++ .idea/Ex4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Ex4 {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String noun = "";
+        String verb = "";
+        String adj = "";
+        String adv = "";
+        System.out.println("Enter a noun: ");
+        noun = kb.next();
+        System.out.println("Enter a verb: ");
+        noun = kb.next();
+        System.out.println("Enter a noun: ");
+        adj = kb.next();
+        System.out.println("Do you " + verb + " your " + adj + noun + " " + adv + " ?");
+
+    }
+}
