6b3fa83daff15245d226f10778bb475fef947791
HarshBrownsVI <harshaaitharaju15@gmail.com>
2021-06-05 14:08:08 -0400

    Exercise 2

--- .idea/Ex2.java
+++ .idea/Ex2.java
@@ -1,9 +1,19 @@
+import java.util.*;
 public class Ex2 {
 
     public static void main(String[] args)
     {
-        
-
+        Scanner kb = new Scanner(System.in);
+        String s = "";
+        System.out.println("What is the input string?");
+        s = kb.next();
+        while( s.equals(" ")){
+            System.out.println("What is the input string?");
+            s = kb.next();
+        }
+        System.out.println(s + " has " + s.length() + " characters.") ;
     }
+
+}
 
 }
