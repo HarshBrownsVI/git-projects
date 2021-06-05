f22f20ca937bf1b46390dc82baf1fe19e51ccc14
HarshBrownsVI <harshaaitharaju15@gmail.com>
2021-06-05 13:12:01 -0400

    Exercise 1

---
+++ .idea/Ex1.java
@@ -0,0 +1,14 @@
+
+public class Ex1{
+
+    public static void main(String[]args)
+    {
+        Scanner kb = new Scanner(System.in);
+        String s = "";
+        System.out.println("What is your name?");
+        s = kb.next();
+        System.out.println("Hello, " + s + " nice to meet you!");
+    }
+
+
+        }
